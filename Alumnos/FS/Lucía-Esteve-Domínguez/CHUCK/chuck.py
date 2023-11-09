78e6a0922d7870d6dc10f45d8066df3b8e9b0b92
Luesdo <lucia5893@gmail.com>
2023-11-09 18:55:22 +0100

    Chuck Norris
    Ejercicio Chuck Norris sin acabar

---
+++ Alumnos/FS/Lucía-Esteve-Domínguez/CHUCK/chuck.py
@@ -0,0 +1,14 @@
+#print("¡Hola Mundo!")
+
+#Importar librerias para hacer la llamada --> ENTORNO
+import  requests
+from requests.models import Response
+url: str = "https://api.chucknorris.io"
+
+#sacar un chiste
+
+
+
+
+#hacer un bucle que cada 5 segundos pides la broma y ves que palabras salen
+#While (True) time.sleep (5)
---
+++ Alumnos/FS/Lucía-Esteve-Domínguez/CHUCK/chuck.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Hola Mundo!
+mkdir carpeta1
+python3 chuck.py
