e28508922fa330c94c35c9f3db493e35d33bd116
FernandoCabreraCarbonell <fernandocabreracarbonell@gmail.com>
2023-11-09 18:55:23 +0100

    repaso_jueves

---
+++ Alumnos/FS/FERNANDO-CABRERA-CARBONELL/CHUCK/repaso.py
@@ -0,0 +1,18 @@
+import requests
+from requests.models import Response
+URL : str = "https://api.chucknorris.io/jokes/random"
+
+lista_chistes = []
+
+#DEFINICIÓN DE FUNCIÓN
+def chistes_Chuck_norris ():
+    f = open("results.txt", "w")
+    for numero in range (1,11):
+                respuesta : Response = requests.get(URL)
+                datos = respuesta.json()
+                frase_chuck: str = datos["value"]
+                f.write(f'{frase_chuck}\n')
+
+#EJECUCIÓN DE FUNCIÓN
+chistes_Chuck_norris()
+
---
+++ Alumnos/FS/FERNANDO-CABRERA-CARBONELL/CHUCK/script.sh
@@ -0,0 +1,2 @@
+mkdir results
+python3 repaso.py
