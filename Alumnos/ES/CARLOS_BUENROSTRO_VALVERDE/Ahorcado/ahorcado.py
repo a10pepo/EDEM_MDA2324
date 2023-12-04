bcf79c67fd6cf6487ce68137f9d4dd35262d66ce
cbuenrostrovalverde <148787241+cbuenrostrovalverde@users.noreply.github.com>
2023-12-04 10:32:57 +0100

    Update ahorcado.py

--- Alumnos/ES/CARLOS_BUENROSTRO_VALVERDE/Ahorcado/ahorcado.py
+++ Alumnos/ES/CARLOS_BUENROSTRO_VALVERDE/Ahorcado/ahorcado.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 palabras = pd.read_csv('palabras.csv')["PALABRAS"]
 lista_palabras = list(palabras)
+abecedario = ["E", "A", "O", "S", "R", "N", "I", "D", "L", "C", "T", "U", "M", "P", "B", "G", "V", "Y", "Q", "H", "F", "Z", "J", "Ñ", "X", "K", "W"]
 
 for palabra in lista_palabras:
     letras = len(palabra)
